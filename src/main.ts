45506670f93a371691cc7355917bdffa74c23a1b
Shaedow2000 <amsal8795@gmail.com>
2026-01-18 12:22:13 +0100

    Complete photographs for mobile devices.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 const second_header = document.getElementById( 'second-header' ) as HTMLHeadingElement;
+const third_header  = document.getElementById(  'third-header' ) as HTMLHeadingElement;
+
+const second_text = document.getElementById( 'second-text' ) as HTMLParagraphElement;
 
 function is_visible( element: HTMLElement ): boolean {
   const values = element.getBoundingClientRect()
@@ -11,5 +14,20 @@ function is_visible( element: HTMLElement ): boolean {
 self.addEventListener( 'scroll', (): void => {
   if ( is_visible( second_header ) ) {
     second_header.classList.add( 'animate-appear' )
+    second_text.classList.add( 'animate-appear' )
+  }
+
+  if ( is_visible(  third_header ) ) {
+    third_header.classList.add( 'animate-appear' )
+  }
+  
+  for ( let i: number = 0; i <= 9; i++ ) {
+    const element = document.getElementById( `p${ i + 1 }` ) as HTMLElement;
+
+    if ( is_visible( element ) ) {
+      element.style.scale = '105%';
+    } else {
+      element.style.scale = '100%';
+    }
   }
 } )
