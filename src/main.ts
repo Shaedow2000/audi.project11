094c0c12321997dd242efe0623e7beefaadd63e5
Shaedow2000 <amsal8795@gmail.com>
2026-01-18 13:36:23 +0100

    Create footer section.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 const second_header = document.getElementById( 'second-header' ) as HTMLHeadingElement;
 const third_header  = document.getElementById(  'third-header' ) as HTMLHeadingElement;
-
+const fourth_header = document.getElementById( 'fourth-header' ) as HTMLHeadingElement;
+ 
 const second_text = document.getElementById( 'second-text' ) as HTMLParagraphElement;
+const third_text  = document.getElementById(  'third-text' ) as HTMLParagraphElement;
 
 function is_visible( element: HTMLElement ): boolean {
   const values = element.getBoundingClientRect()
@@ -13,12 +15,17 @@ function is_visible( element: HTMLElement ): boolean {
 
 self.addEventListener( 'scroll', (): void => {
   if ( is_visible( second_header ) ) {
-    second_header.classList.add( 'animate-appear' )
-    second_text.classList.add( 'animate-appear' )
+    second_header.classList.add( 'animate-appear' );
+    second_text.classList.add( 'animate-appear' );
   }
 
   if ( is_visible(  third_header ) ) {
-    third_header.classList.add( 'animate-appear' )
+    third_header.classList.add(  'animate-appear' );
+  }
+
+  if ( is_visible( fourth_header ) ) {
+    fourth_header.classList.add( 'animate-appear' );
+    third_text.classList.add( 'animate-appear' );
   }
   
   for ( let i: number = 0; i <= 9; i++ ) {
