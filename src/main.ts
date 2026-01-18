4f25609d295b99931c73664121fd6ad6b686a853
Shaedow2000 <amsal8795@gmail.com>
2026-01-18 10:42:48 +0100

    Create is_visible().

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-screen.orientation.lock( 'portrait' )
+const second_header = document.getElementById( 'second-header' ) as HTMLHeadingElement;
+
+function is_visible( element: HTMLElement ): boolean {
+  const values = element.getBoundingClientRect()
+  return (
+    values.top >= 0 &&
+    values.bottom <= self.innerHeight
+  ) 
+}
+
